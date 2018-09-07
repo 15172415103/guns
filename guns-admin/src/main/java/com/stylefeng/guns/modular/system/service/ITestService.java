836293b054c299fcffed849082b9b4af90fb2085
YJ123 <15172415103@163.com>
2018-09-07 10:38:14 +0800

    mysql地址修改

---
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/system/service/ITestService.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.modular.system.service;
+
+import com.stylefeng.guns.modular.system.model.Test;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author stylefeng
+ * @since 2018-09-06
+ */
+public interface ITestService extends IService<Test> {
+
+}
